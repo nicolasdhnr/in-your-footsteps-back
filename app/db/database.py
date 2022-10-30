694e53efa27642d6774f1035b9c8cef14b5b00f1
t59767ct <celine.tandiono@student.manchester.ac.uk>
2022-10-30 14:11:00 +0000

    changes

--- app/db/database.py
+++ app/db/database.py
@@ -5,48 +5,37 @@ import json
 from datetime import date
 import os
 
+pwd = os.getenv('DB_PASS')
+user = os.getenv('DB_USER')
+ip = os.getenv('DB_IP')
+engine = sqlalchemy.create_engine(f'mysql+pymysql://root:{pwd}@{ip}')
 
-class data_to_db():
-    def __init__(self):
-        self.establish_connection()
+def send_to_db(table, json_file):
+    data = json.load(json_file)
+    for i in data:
+        values = list(i.values())
+        if table == 'authentication':
+            engine.execute(self.insert_auth(values))
+        elif table == 'profile':
+            engine.execute(self.insert_profile(values))
+        elif table == 'story':
+            engine.execute(self.insert_story(values))
+        elif table == 'path':
+            engine.execute(self.insert_path(values))
+        elif table == 'voice':
+            engine.execute(self.insert_voice(values))
 
-    def establish_connection(self):
-        pwd = os.getenv('DB_PASS')
-        user = os.getenv('DB_USER')
-        ip = os.getenv('DB_IP')
-        engine = sqlalchemy.create_engine(f'mysql+pymysql://root:{pwd}@{ip}')
+def insert_auth(data):
+    return f"INSERT INTO footsteps_db.authentication (userUID, identifier, provider, created, signed_in) VALUES({str(data[0])}, '{data[1]}', '{data[2]}', {str(data[3])}, {str(data[4])})"
 
-    def send_to_db(self, table, json_file):
-        data = json.load(json_file)
-        for i in data:
-            values = list(i.values())
-            if table == 'authentication':
-                engine.execute(self.insert_auth(values))
-            elif table == 'profile':
-                engine.execute(self.insert_profile(values))
-            elif table == 'story':
-                engine.execute(self.insert_story(values))
-            elif table == 'path':
-                engine.execute(self.insert_path(values))
-            elif table == 'voice':
-                engine.execute(self.insert_voice(values))
+def insert_profile(data):
+    return f"INSERT INTO footsteps_db.profile (user_id, userUID, username) VALUES ({str(data[0])}, {str(data[1])}, {data[2]})"
 
-    def insert_auth(self, data):
-        return f"INSERT INTO footsteps_db.authentication (userUID, identifier, provider, created, signed_in) VALUES({str(data[0])}, '{data[1]}', '{data[2]}', {str(data[3])}, {str(data[4])})"
+def insert_story(data):
+    return f"INSERT INTO footsteps_db.story (story_id, user_id, title, ranking, theme) VALUES ({str(data[0])}, {str(data[1])}, '{data[2]}', '{data[3]}', '{data[4]}')"
 
-    def insert_profile(self, data):
-        return f"INSERT INTO footsteps_db.profile (user_id, userUID, username) VALUES ({str(data[0])}, {str(data[1])}, {data[2]})"
+def insert_path(data):
+    return f"INSERT INTO footsteps_data.path (path_id, story_id, latitude, longitude, timestamp) VALUES ({str(data[0])}, {str(data[1])}, {str(data[2])}, {str(data[3])}, {str(data[4])})"
 
-    def insert_story(self, data):
-        return f"INSERT INTO footsteps_db.story (story_id, user_id, title, ranking, theme) VALUES ({data})"
-
-    def insert_path(self, data):
-        return f"INSERT INTO footsteps_data.path (path_id, story_id, latitude, longitude, timestamp) VALUES ({data})"
-
-    def insert_voice(self, data):
-        return f"INSERT INTO footsteps_data.voice(voice_id, story_id, order, text, start, end) VALUES ({data})"
-
-
-db = data_to_db()
-with open('prod.json') as json_file:
-    db.send_to_db('authentication', json_file)
+def insert_voice(data):
+    return f"INSERT INTO footsteps_data.voice(voice_id, story_id, order, text, start, end) VALUES ({str(data[0])}, {str(data[1])}, {str(data[2])}, '{data[3]}', {str(data[4])}, {str(data[5])})"
--- app/db/manipulation.py
+++ app/db/manipulation.py
@@ -4,25 +4,35 @@ from sqlalchemy import insert
 import json
 from datetime import date
 import os
+import pandas as pd
+
+pwd = os.getenv('DB_PASS')
+user = os.getenv('DB_USER')
+ip = os.getenv('DB_IP')
+engine = sqlalchemy.create_engine('mysql+mysqlconnector://root:inyourfootsteps@35.246.2.66')
+conn = engine.connect()
 
 
-class data_to_db():
-    def __init__(self):
-        self.establish_connection()
+def get_nearest_ten(lat, lng):
+    query = f"SELECT story_id, startlat, startlng, SQRT(POW(69.1 * (startlat - {lat}), 2) + POW(69.1 * ({lng} - startlng) * COS(startlat / 57.3), 2)) AS distance  FROM story ORDER BY distance LIMIT 0, 10;"
+    engine.execute(query)
+    for i in result:
+        print(i['path_id'])
 
-    def establish_connection(self):
-        pwd = os.getenv('DB_PASS')
-        user = os.getenv('DB_USER')
-        ip = os.getenv('DB_IP')
-        engine = sqlalchemy.create_engine(f'mysql+pymysql://root:{pwd}@{ip}')
+def get_profile_data(username):
+    query = f"SELECT * FROM `profile`LEFT JOIN `story` ON `profile`.`user_id`=`story`.`user_id` WHERE `profile`.`username`='{username}' UNION SELECT * FROM `profile` RIGHT JOIN `story` ON `profile`.`user_id`=`story`.`user_id` WHERE `profile`.`username`='{username}';"
+    result = engine.execute(query)
 
-    def get_nearest_ten(self, lat, lng):
-        query = f'SELECT path_id, latitude, longitude, SQRT(POW(69.1 * (latitude - {lat}), 2) + POW(69.1 * ({lng} - longitude) * COS(latitude / 57.3), 2)) AS distance  FROM path ORDER BY distance LIMIT 0, 1000'
-        # TODO: Also needs to order by ranking within those 1000.
-        engine.execute(query)
+def search_by_title(title):
+    query = f"SELECT * FROM `story` WHERE `title`='{title}'"
+    result = engine.execute(query)
+    for i in result:
+        print(i['title'])
 
+def search_by_theme(title):
+    query = f"SELECT * FROM `story` WHERE `title`='{title}'"
+    result = engine.execute(query)
 
-
-def get_recommended_paths(lat, lng):
-    #
-    pass
+def upvote(story_id):
+    query = f"UPDATE story SET ranking = ranking + 1 WHERE story_id ={story_id}"
+    result = engine.execute(query)
