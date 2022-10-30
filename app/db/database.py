de8beb04be3261baea5396479a3346fb276aef75
t59767ct <celine.tandiono@student.manchester.ac.uk>
2022-10-30 01:11:41 +0100

    insert

--- app/db/database.py
+++ app/db/database.py
@@ -3,13 +3,17 @@ import pymysql
 from sqlalchemy import insert
 import json
 from datetime import date
+import os
 
 class data_to_db():
     def __init__(self):
-        self.establish_connection
+        self.establish_connection()
 
-    def establish_connection():
-        engine = sqlalchemy.create_engine('mysql+pymysql://root:inyourfootsteps@35.246.2.66')
+    def establish_connection(self):
+        pwd = os.getenv('DB_PASS')
+        user = os.getenv('DB_USER')
+        ip = os.getenv('DB_IP')
+        engine = sqlalchemy.create_engine(f'mysql+pymysql://root:{pwd}@{ip}')
 
     def send_to_db(self,table, json_file):
         data = json.load(json_file)
@@ -33,14 +37,10 @@ class data_to_db():
         return f"INSERT INTO footsteps_db.profile (user_id, userUID, username) VALUES ({str(data[0])}, {str(data[1])}, {data[2]})"
 
     def insert_story(self,data):
-        return f"INSERT INTO footsteps_db.story (story_id, user_id, title, ranking, theme) VALUES ({data})"
+        return f"INSERT INTO footsteps_db.story (story_id, user_id, title, ranking, theme) VALUES ({str(data[0])}, {str(data[1])}, '{data[2]}', '{data[3]}', '{data[4]}')"
 
     def insert_path(self,data):
-        return f"INSERT INTO footsteps_data.path (path_id, story_id, latitude, longitude, timestamp) VALUES ({data})"
+        return f"INSERT INTO footsteps_data.path (path_id, story_id, latitude, longitude, timestamp) VALUES ({str(data[0])}, {str(data[1])}, {str(data[2])}, {str(data[3])}, {str(data[4])})"
 
     def insert_voice(self,data):
-        return f"INSERT INTO footsteps_data.voice(voice_id, story_id, order, text, start, end) VALUES ({data})"
-
-db = data_to_db()
-with open('prod.json') as json_file:
-    db.send_to_db('authentication', json_file)
+        return f"INSERT INTO footsteps_data.voice(voice_id, story_id, order, text, start, end) VALUES ({str(data[0])}, {str(data[1])}, {str(data[2])}, '{data[3]}', {str(data[4])}, {str(data[5])})"
